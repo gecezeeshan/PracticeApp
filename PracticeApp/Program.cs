942db7c2df684f0123b2216c600f11264f4b0a93
Zeeshan <gecezeeshan@gmail.com>
2021-06-17 07:11:43 +0500

    submit 2

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -8,18 +8,47 @@ namespace PracticeApp
     {
         static void Main(string[] args)
         {
-            ShapeFactory sh = new ShapeFactory();
-            IShape s1 = sh.CreateShape("square");
-            s1.Draw().Print();
-            s1 = sh.CreateShape("circle");
-            //s1.BorderColor("red").Print();
-            //s1.FillColor("green").Print();
-            s1.AreaCalculator().ToString().Print();
+            //ShapeFactory sh = new ShapeFactory();
+            //IShape s1 = sh.CreateShape("square");
+            //s1.Draw().Print();
+            //s1 = sh.CreateShape("circle");
+            ////s1.BorderColor("red").Print();
+            ////s1.FillColor("green").Print();
+            //s1.AreaCalculator().ToString().Print();
 
-            Fruit fruit = new Orange();
-            fruit.GetColor().Print();
-            fruit = new Apple();
-            fruit.GetColor().Print();
+            //Fruit fruit = new Orange();
+            //fruit.GetColor().Print();
+            //fruit = new Apple();
+            //fruit.GetColor().Print();
+
+            //Test t = new Test();
+            //t.Test1();
+            //t.Test2();
+            //t.test2();
+
+            //Animal a = new Bird();
+            //a.move();
+            //a.Eat();
+            //a.Drink();
+            //Animal a1 = new Fish();
+            //a1.move();
+            //a1.Eat();
+            //a1.Drink();
+
+
+            //DuplicateFileRemover duplicateFileRemover = new DuplicateFileRemover();
+            //duplicateFileRemover.DuplicateRemover();
+
+            BinaryGap b = new BinaryGap();
+
+            Console.WriteLine(string.Format("gaps are {0}", b.FindGap(1041)));
+            Console.WriteLine(string.Format("gaps are {0}", b.FindGap(529)));
+            Console.WriteLine(string.Format("gaps are {0}", b.FindGap(32)));
+
+            Console.WriteLine(string.Format("gaps are {0}", b.FindGap(2342)));
+
+
+
         }
 
 
---
+++ PracticeApp/Solid/InterfaceTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeApp.Solid
+{
+    public interface InterfaceTest
+    {
+        void Test3();
+    }
+
+    public interface IInterace2 : InterfaceTest
+    {
+        void test2();
+    }
+
+    public abstract class AbstractTest
+    {
+        public abstract void Test1();
+    }
+
+    public abstract class AbstractTest1 : AbstractTest
+    {
+        public abstract void Test2();
+    }
+
+    public class Test : AbstractTest1, IInterace2
+    {
+        public override void Test1()
+        {
+            Console.WriteLine("I am from AbstractTest");
+        }
+        public override void Test2()
+        {
+            Console.WriteLine("I am from AbstractTest1");
+        }
+
+        void InterfaceTest.Test3()
+        {
+            Console.WriteLine("I am from InterfaceTest");
+        }
+
+        public void test2()
+        {
+            Console.WriteLine("I am from Interface2");
+            // InterfaceTest.Test3();
+        }
+    }
+
+
+    public abstract class Animal
+    {
+        public void move() { Console.WriteLine("Animal move"); }
+        public virtual void Eat() { Console.WriteLine("Eat"); }
+        public void Drink() { Console.WriteLine("Drink"); }
+    }
+
+    public class Bird : Animal
+    {
+        public new void move() { Console.WriteLine("Fly"); }
+    }
+
+    public class Fish : Animal
+    {
+        public new void move() { Console.WriteLine("Swim"); }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Swallow");
+        }
+
+        public new void Drink() { Console.WriteLine("Fish Drink"); }
+    }
+
+}
